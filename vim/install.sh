275ad4c4ce4ab74a10b10ce55510560a549f0d52
Ticean Bennett <ticean@gmail.com>
2017-11-20 10:27:25 -0800

    Cleaner Vim Bundle install and output

--- vim/install.sh
+++ vim/install.sh
@@ -1,10 +1,17 @@
-vim_bundle_dir=$HOME/.vim/bundle
+vim_bundle_dir="$HOME/.vim/bundle"
 mkdir -p "$vim_bundle_dir"
 
 if [ -d "$vim_bundle_dir/vundle" ]; then
-  echo "Vim Vundle directory already exists, not cloning with git."
+  echo "Vim Vundle directory already exists, not cloning with git. Using existing repo."
 else
   git clone https://github.com/gmarik/vundle "$vim_bundle_dir/vundle"
 fi
 
-$HOME/bin/vim -c "BundleInstall" -c "q" -c "q"
+echo "Cleaning Vim Bundle."
+vim -c "BundleClean" -c "q" -c "q"
+
+echo "Installing Vim Bundle."
+vim -c "BundleInstall" -c "q" -c "q"
+
+echo "Updating Vim Bundle."
+vim -c "BundleUpdate" -c "q" -c "q"
