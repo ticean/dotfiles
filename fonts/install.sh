637560bc4eaeafe34c4537a9757ed1e9ba1553d5
Ticean Bennett <ticean@gmail.com>
2017-11-28 22:24:59 -0800

    Distribute fonts

---
+++ fonts/install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# Install fonts on Mac OSX.
+cp -R "$DOTFILES/fonts/source-code-pro/OTF/." /Library/Fonts
+cp -R "$DOTFILES/fonts/source-code-pro-powerline/OTF/." /Library/Fonts
