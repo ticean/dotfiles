e4dedc6658ce926d6758f37349d8e38b6ff18bf3
Ticean Bennett <ticean@gmail.com>
2017-11-28 23:19:14 -0800

    reaction

---
+++ reactioncommerce/install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# ReactionCommerce specific installations.
+
+# Configure Atom and tools per the ReactionCommerce core team.
+#
+# See: https://docs.reactioncommerce.com/reaction-docs/master/styleguide
+apm install \
+  editorconfig \
+  atom-beautify \
+  linter \
+  linter-eslint \
+  linter-markdown \
+  linter-jsonlint \
+  linter-docker
