3dfccff7d787bab62d800249f9e1392e973de0a2
Ticean Bennett <ticean@gmail.com>
2018-03-24 10:10:08 -0700

    Configure virtualenv for python3

--- python/exports.zsh
+++ python/exports.zsh
@@ -1,6 +1,7 @@
-# virtualenvwrapper
-export WORKON_HOME=$HOME/.virtualenvs
+# virtualenvwrapper - configured for Python3
+export VIRTUALENVWRAPPER_PYTHON="$(which python3)"
 export VIRTUALENVWRAPPER_VIRTUALENV_ARGS='--no-site-packages'
+export WORKON_HOME=$HOME/.virtualenvs
 
 # PIP to work with virtualenv
 export PIP_VIRTUALENV_BASE=$WORKON_HOME
