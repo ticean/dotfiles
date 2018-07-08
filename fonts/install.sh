22db853ee5a4a106481bcd9fd1de978e8d7f6b28
Ticean Bennett <ticean@gmail.com>
2018-07-08 15:40:51 -0700

    Switch to Hasklig font

--- fonts/install.sh
+++ fonts/install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # Install fonts on Mac OSX.
-cp -R "$DOTFILES/fonts/source-code-pro/OTF/." /Library/Fonts
+cp -R "$DOTFILES/fonts/hasklig-1.1/." /Library/Fonts
+cp -R "$DOTFILES/fonts/source-code-pro-powerline/OTF/." /Library/Fonts
 cp -R "$DOTFILES/fonts/source-code-pro-powerline/OTF/." /Library/Fonts
