56d24e716a71c0b80aee26cc264c20231e35c295
Ticean Bennett <ticean@gmail.com>
2020-04-21 14:06:11 -0700

    check for kube tools before using

--- kubernetes/completion.zsh
+++ kubernetes/completion.zsh
@@ -1,3 +1,11 @@
-source <(helm completion zsh)
-source <(kubectl completion zsh)
-source <(minikube completion zsh)
+if command -v helm >/dev/null 2>&1; then
+  source <(helm completion zsh)
+fi
+
+if command -v kubectl >/dev/null 2>&1; then
+  source <(kubectl completion zsh)
+fi
+
+if command -v minikube >/dev/null 2>&1; then
+  source <(minikube completion zsh)
+fi
