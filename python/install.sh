db7876a426207d7754f87396f00244a9c979ca58
Ticean Bennett <ticean@gmail.com>
2020-04-23 22:13:58 -0700

    python installation

--- python/install.sh
+++ python/install.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
-#
-pip3 install --upgrade pip
+# Configure Python3
 
-pip3 install virtualenv
-pip3 install --upgrade virtualenvwrapper
+if test "$(command -v pip3)"; then
+  pip3 install --upgrade pip3
+  pip3 install --upgrade setuptools
+  CFLAGS=-I$(brew --prefix)/include LDFLAGS=-L$(brew --prefix)/lib pip3 install virtualenv
+  CFLAGS=-I$(brew --prefix)/include LDFLAGS=-L$(brew --prefix)/lib pip3 install --upgrade virtualenvwrapper
+fi
+
+
+# Configure Python2
+# This version is no longer supported but ships on OSX as the system python.
+if ! test "$(command -v pip3)"; then
+  curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py \
+    && python2 /tmp/get-pip.py \
+    && rm /tmp/git-pip.py
+else
+  python2 -m pip install --upgrade setuptools
+  python2 -m pip install --upgrade virtualenv
+  python2 -m pip install --upgrade virtualenvwrapper
+fi
