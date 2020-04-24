09d2cf37fe45a1d91dd1c88c74b69be8d4ce31fd
Ticean Bennett <ticean@gmail.com>
2020-04-23 20:43:41 -0700

    must have colors
    this wasn't required before but this works and I gotta have them colors

--- colorscheme/base16-shell.zsh
+++ colorscheme/base16-shell.zsh
@@ -4,3 +4,5 @@ BASE16_SHELL=$HOME/.config/base16-shell/
 [ -n "$PS1" ] \
   && [ -s "$BASE16_SHELL/profile_helper.sh" ] \
   && eval "$("$BASE16_SHELL/profile_helper.sh")"
+
+source $HOME/.config/base16-shell/scripts/base16-eighties.sh
